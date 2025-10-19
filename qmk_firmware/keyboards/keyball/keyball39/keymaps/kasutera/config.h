6f7a2efd5bc5d7850c3ef8a168caf491e8345f2b
bwxzgeretou <bwxzgeretou@gmail.com>
2025-10-19 12:36:53 +0900

    minimize diff to via

--- qmk_firmware/keyboards/keyball/keyball39/keymaps/kasutera/config.h
+++ qmk_firmware/keyboards/keyball/keyball39/keymaps/kasutera/config.h
@@ -21,11 +21,11 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #pragma once
 
 #ifdef RGBLIGHT_ENABLE
-#    define RGBLIGHT_EFFECT_BREATHING
-#    define RGBLIGHT_EFFECT_RAINBOW_MOOD
-#    define RGBLIGHT_EFFECT_RAINBOW_SWIRL
+//#    define RGBLIGHT_EFFECT_BREATHING
+//#    define RGBLIGHT_EFFECT_RAINBOW_MOOD
+//#    define RGBLIGHT_EFFECT_RAINBOW_SWIRL
 //#    define RGBLIGHT_EFFECT_SNAKE
-#    define RGBLIGHT_EFFECT_KNIGHT
+//#    define RGBLIGHT_EFFECT_KNIGHT
 //#    define RGBLIGHT_EFFECT_CHRISTMAS
 #    define RGBLIGHT_EFFECT_STATIC_GRADIENT
 //#    define RGBLIGHT_EFFECT_RGB_TEST
@@ -33,12 +33,12 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 //#    define RGBLIGHT_EFFECT_TWINKLE
 #endif
 
+#define TAP_CODE_DELAY 5
+
 // #define TAPPING_TERM 150 // HOLD判定までのms
 
-// #define POINTING_DEVICE_AUTO_MOUSE_ENABLE // 有効化
-// #define AUTO_MOUSE_DEFAULT_LAYER 2 // 切り替えるマウスレイヤー番号を指定
-// #define AUTO_MOUSE_TIME 500 // マウスが止まってから元のレイヤーに戻るまでの時間(ms)
+#define POINTING_DEVICE_AUTO_MOUSE_ENABLE // 有効化
+#define AUTO_MOUSE_DEFAULT_LAYER 2 // 切り替えるマウスレイヤー番号を指定
+#define AUTO_MOUSE_TIME 500 // マウスが止まってから元のレイヤーに戻るまでの時間(ms)
 
 #define KEYBALL_CPI_DEFAULT 1300 // マウス速度 (default: 500)
-
-#define TAP_CODE_DELAY 5
