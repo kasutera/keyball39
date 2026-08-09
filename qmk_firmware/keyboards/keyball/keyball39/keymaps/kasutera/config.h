9dec6e34ce5cb758dc576b56b35450734c9ea54d
bwxzgeretou <bwxzgeretou@gmail.com>
2026-08-09 11:36:35 +0900

    remove aml related variables and layer_state_set_user

--- qmk_firmware/keyboards/keyball/keyball39/keymaps/kasutera/config.h
+++ qmk_firmware/keyboards/keyball/keyball39/keymaps/kasutera/config.h
@@ -37,8 +37,4 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 // #define TAPPING_TERM 150 // HOLD判定までのms
 
-#define POINTING_DEVICE_AUTO_MOUSE_ENABLE // 有効化
-#define AUTO_MOUSE_DEFAULT_LAYER 2 // 切り替えるマウスレイヤー番号を指定
-#define AUTO_MOUSE_TIME 500 // マウスが止まってから元のレイヤーに戻るまでの時間(ms)
-
 #define KEYBALL_CPI_DEFAULT 900 // マウス速度 (default: 500)
